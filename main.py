bf8e086285204b9f6f2b151084005927145a0d50
Nikhil CSB <csbnikhil@gmail.com>
2021-03-28 03:20:38 +0530

    create css with colors

--- main.py
+++ main.py
@@ -12,5 +12,11 @@ def make_root(json_scheme):
         ';'.join(starmap('--{}:{}'.format, json_scheme.items()))
     )
 
+def export_css(dest, json_scheme):
+    with open(dest, 'w') as export_file:
+        export_file.write(make_root(json_scheme))
+
+
 print(make_root(load_colors('colors/everforest.json')))
+export_css('css/scheme.css', load_colors('colors/everforest.json'))
 
