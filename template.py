f0ce1be2495592fe2f7c69cb212a4b994a3d67cc
Nikhil CSB <csbnikhil@gmail.com>
2021-03-28 11:42:28 +0530

    add string template

---
+++ template.py
@@ -0,0 +1,25 @@
+'''\
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+
+  <!-- colorscheme -->
+  <link rel="stylesheet" href="css/scheme.css">
+  <!-- styles -->
+  <link rel="stylesheet" href="css/center.css">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦄</text></svg>">
+
+  <title>The Snippet</title>
+</head>
+<body>
+<pre class="code-canvas">
+<code id="snippet">
+{}
+</code>
+</pre>
+</body>
+</html>'''
