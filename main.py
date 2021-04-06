3655e1bda08da3c401cd7fa5a63158b929cfc16c
Nikhil CSB <csbnikhil@gmail.com>
2021-04-07 02:02:20 +0530

    define main

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 import json
 from itertools import starmap
 
-def load_colors(cs_name):
-    with open(cs_name) as scheme:
+def load_colors(colorscheme_file):
+    with open(colorscheme_file) as scheme:
         return json.load(scheme)
 
 def make_root(json_scheme):
@@ -23,10 +23,13 @@ def render(dest, template, snippet):
     with open(dest, 'w') as html_file:
         html_file.write(html)
 
+def main():
+	print(make_root(load_colors('colors/everforest.json')))
+	export_css('css/scheme.css', load_colors('colors/everforest.json'))
 
-print(make_root(load_colors('colors/everforest.json')))
-export_css('css/scheme.css', load_colors('colors/everforest.json'))
+	with open('code.py') as snippet_file:
+		render('index.html', 'template.html', snippet_file.read().strip())
 
-with open('code.py') as snippet_file:
-    render('index.html', 'template.html', snippet_file.read().strip())
 
+if __name__ == '__main__':
+	main()
